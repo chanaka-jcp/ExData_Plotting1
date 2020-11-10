01472a29a86f86f9011962a0ee5a210c1e7bf91b
chanaka-jcp <gunawardhanajcp@gmail.com>
2020-11-10 13:21:09 +0530

    1st plot

---
+++ plot1.R
@@ -0,0 +1,6 @@
+data_t<-read.table("household_power_consumption.txt",sep=";",header=TRUE)
+selected_data<-data_t[data_t$Date=="1/2/2007"|data_t$Date=="2/2/2007",]
+selected_data$Global_active_power <- as.numeric(selected_data$Global_active_power)
+hist(selected_data$Global_active_power,main="Global Active Power",xlab = "Global Active Power(kilowatts)",col="red")
+dev.copy(png,file="plot1.png")
+dev.off()
